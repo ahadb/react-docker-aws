6589ef30d2060ca4231f4c7299fe4f8cdb4c7f4c
ahadb <ahadbokhari@gmail.com>
2023-09-09 12:25:24 -0400

    dev1 changed something on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dev1 changes this on the feature branch
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React - Deployed to AWS Beanstalk
+          Learn React
         </a>
       </header>
     </div>
